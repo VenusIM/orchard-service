3495ade040107f8252d06d6abf5fd1ce7f75f9bf
imjunhui <yim3370@gmail.com>
2024-04-29 02:51:06 +0900

    feat : 소개 페이지 문의 1차 퍼블리싱 완료

--- src/main/java/com/orchard/domain/MainController.java
+++ src/main/java/com/orchard/domain/MainController.java
@@ -9,8 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String index(Model model) {
-        log.info("index page");
+    public String index() {
         return "index";
     }
 }
